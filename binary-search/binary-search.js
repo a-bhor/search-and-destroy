5963f64a453b70807a591ba5eb028ea903f669a6
Archana Bhor <archana.bhor@gmail.com>
2020-10-28 16:20:03 -0400

    Binary search with EC completed

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,74 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
+const binarySearch = (array, target, startIdx=-1, endIdx=-1) => {
+
+	console.log(array, target)
+	let len;
+	 if( startIdx === -1 && endIdx=== -1) {
+		startIdx = 0;
+		endIdx = array.length - 1;
+		len = array.length;
+	 } else {
+		 len = endIdx-startIdx + 1;
+	 }
 	
+	console.log("startIdx : ", startIdx, " endIdx: ", endIdx)
+	
+	if(!len) {
+		return false;
+	}
+
+	if (len === 1){
+		return (array[startIdx] === target);
+	}
+
+	let midPoint = startIdx + Math.floor(len/2);
+	let midPointValue = array[midPoint]
+	console.log("midPoint : ", midPoint)
+
+	if( midPointValue === target) {
+		return true;
+	}
+
+	if (midPointValue < target) {
+		return binarySearch (array, target, midPoint+1, endIdx )
+
+	} else {
+		return binarySearch(array, target, startIdx, midPoint-1)
+	}
+
+	return false;
 };
+// const binarySearch = (array, target) => {
+
+// 	// console.log(array, target)
+// 	let len = array.length;
+	
+// 	if(!len) {
+// 		return false;
+// 	}
+
+// 	if (len === 1){
+// 		return (array[0] === target);
+// 	}
+
+// 	let midPoint = Math.floor(len/2);
+// 	let midPointValue = array[midPoint]
+
+// 	if( midPointValue === target) {
+// 		return true;
+// 	}
+
+// 	if (midPointValue < target) {
+// 		return binarySearch (array.slice(midPoint+1 ), target)
+
+// 	} else {
+// 		return binarySearch(array.slice(0, midPoint), target)
+// 	}
+
+// 	return false;
+// };
 
 /*
 	EXTRA CREDIT:
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -5,6 +5,8 @@ const binarySearch = require('./binary-search');
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
+const test4 = [-5, 7, 15, 18];
+const test5 = [5, 7, 15, 18, 21];
 
 
 describe('Binary Search', () => {
@@ -13,6 +15,7 @@ describe('Binary Search', () => {
 		expect(binarySearch(test2, -3)).to.equal(true);
 		expect(binarySearch(test2, 20)).to.equal(true);
 		expect(binarySearch(test3, 2.6)).to.equal(true);
+		expect(binarySearch(test4, -5)).to.equal(true);
 	});
 
 	it('returns false if target value is not in array', () => {
